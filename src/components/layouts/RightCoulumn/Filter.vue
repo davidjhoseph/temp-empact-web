7351cdc5ffb7e920cf2aa1298886ddd35557cc77
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2023-05-26 13:38:01 +0100

    make calendar on top of content in grid view

--- src/components/layouts/RightCoulumn/Filter.vue
+++ src/components/layouts/RightCoulumn/Filter.vue
@@ -17,11 +17,7 @@
       leave-to-class="transform scale-95 opacity-0"
     >
       <MenuItems
-<<<<<<< HEAD
         class="absolute z-20 mt-4 w-82 rounded-s-md rounded-e-md bg-white shadow-lg"
-=======
-        class="absolute mt-4 bg-white shadow-lg w-82 rounded-s-md rounded-e-md"
->>>>>>> 7d1d78c030b12920c1411e299407f11b840f3e08
       >
         <div class="flex items-center justify-between pt-2 ps-4 pe-4">
           <div class="font-bold capitalize text-gray-80">Filter by date</div>
