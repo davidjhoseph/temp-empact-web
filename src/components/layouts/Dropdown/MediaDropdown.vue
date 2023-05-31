8ef26528104ec3c4702018aff28f7c2d1b0aa270
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2023-05-31 20:12:31 +0100

    upload file

--- src/components/layouts/Dropdown/MediaDropdown.vue
+++ src/components/layouts/Dropdown/MediaDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="">
+  <!-- <form action="">
     <div
       class="flex items-center space-s-2 h-12 w-max border border-gray-30 rounded-md bg-white px-2"
     >
@@ -15,8 +15,8 @@
         </option>
       </select>
     </div>
-  </form>
-  <!-- <div class="py-1">
+  </form> -->
+  <div class="py-1">
     <small class="px-4 text-blue" style="">Select content type</small>
     <form action="" class="px-4 pb-2">
       <div class="pt-4">
@@ -40,7 +40,7 @@
         <label for="text_only" class="px-2 pb-2">Text Only</label>
       </div>
     </form>
-  </div> -->
+  </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
